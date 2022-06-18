d9f268d26ca2ca8d8661208f22589dfd947405be
Issabella99 <rivillasisabella@gmail.com>
2022-06-18 08:57:06 -0500

    Creacion programa

--- src/Convertidor.java
+++ src/Convertidor.java
@@ -1,2 +1,25 @@
+import javax.swing.*;
+
 public class Convertidor {
+
+    double pesos;
+    double bolivares = 0.0014;
+    double conversion;
+
+    public Convertidor() {
+        PesosBolivares();
+    }
+
+    public void PesosBolivares() {
+        pesos = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor en pesos "));
+        conversion = pesos * bolivares;
+        JOptionPane.showMessageDialog(null, pesos + " Es igual a " + conversion + " bolivares");
+
+
+    }
+
+    public static void main(String[] args) {
+
+        new Convertidor();
+    }
 }
